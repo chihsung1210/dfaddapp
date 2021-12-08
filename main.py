6b03c6ca1ca858a44e08f4d355c159df0c268874
chihsung1210 <90314290+chihsung1210@users.noreply.github.com>
2021-12-08 10:10:52 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ app = Flask(__name__)
 def index():
         return "<h1>Hello Python using Flask</h1>"
 
+# create a route for webhook
+@app.route('/webhook', methods=['POST'])
+
+def webhook():
+  req = request.get_json(silent=True, force=True)
+  sum = 0
+  query_result = req.get('queryResult')
+  num1 = int(query_result.get('parameters').get('number'))
+  num2 = int(query_result.get('parameters').get('number1'))
+  sum = str(num1 + num2)
+  print('here num1 = {0}'.format(num1))
+  print('here num2 = {0}'.format(num2))
+  return {
+        "fulfillmentText": 'The sum of the two numbers is: '+sum,
+        "source": "webhookdata"
+    }
+                   
+
 #run the app
 if __name__ == '__main__':
    app.run()            
