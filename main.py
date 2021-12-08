34833b7d7f97cb6e108c6f5f8a89898f27564df3
chihsung1210 <90314290+chihsung1210@users.noreply.github.com>
2021-12-08 09:48:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,request
  
 app = Flask(__name__)
  
 @app.route("/")
 def index():
-        return "<h1>Hello World</h1>"
+        return "<h1>Hello Python using Flask</h1>"
 
 #run the app
 if __name__ == '__main__':
